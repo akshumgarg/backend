c94cd4240a6d969955b3a11dc8aeb4f5c520ff2b
Akshum <akshumgarg20@gmail.com>
2026-01-30 00:44:04 +0530

    Add render deployment config

--- build.sh
+++ build.sh
@@ -2,5 +2,9 @@
 set -o errexit
 
 pip install -r requirements.txt
+
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Create superuser (add this line)
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='akshum').exists() or User.objects.create_superuser('akshum', 'akshumgarg20@gmail.com', 'akshum@123')" | python manage.py shell
